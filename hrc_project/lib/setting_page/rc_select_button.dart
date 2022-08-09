8d22440858a8905e874db9ead4fddd476e87b5af
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-08-09 17:43:43 +0900

    RC color update
    RC 별 커튼 색상 지정

--- hrc_project/lib/setting_page/rc_select_button.dart
+++ hrc_project/lib/setting_page/rc_select_button.dart
@@ -95,14 +95,35 @@ Widget rcSelectButton(
 
 //  RC color
 List<Color> rcColors(int index) {
+  //  열송학사
   if (index == 0) {
     return [
-      const Color.fromRGBO(186, 104, 186, 1),
-      const Color.fromRGBO(159, 101, 190, 1),
-      const Color.fromRGBO(147, 99, 201, 1),
-      const Color.fromRGBO(129, 97, 208, 1),
+      const Color.fromRGBO(69, 88, 26, 1),
+      const Color.fromRGBO(86, 108, 43, 1),
+      const Color.fromRGBO(66, 141, 0, 1),
+      const Color.fromRGBO(52, 132, 0, 1),
     ];
-  } else if (index == 1) {
+  }
+  //  손양원
+  else if (index == 1) {
+    return [
+      const Color.fromRGBO(90, 0, 26, 1),
+      const Color.fromRGBO(179, 28, 67, 1),
+      const Color.fromRGBO(201, 90, 63, 1),
+      const Color.fromRGBO(241, 214, 94, 1),
+    ];
+  }
+  //  비전
+  else if (index == 2) {
+    return [
+      const Color.fromRGBO(160, 110, 0, 1),
+      const Color.fromRGBO(200, 131, 0, 1),
+      const Color.fromRGBO(255, 232, 82, 1),
+      const Color.fromRGBO(255, 234, 238, 1),
+    ];
+  }
+  //  무소속
+  else if (index == 3) {
     return [
       const Color.fromRGBO(186, 104, 186, 1),
       const Color.fromRGBO(159, 101, 190, 1),
@@ -111,44 +132,36 @@ List<Color> rcColors(int index) {
       const Color.fromRGBO(76, 93, 220, 1),
       const Color.fromRGBO(61, 90, 230, 1),
     ];
-  } else if (index == 2) {
+  }
+  //  장기려
+  else if (index == 4) {
     return [
-      const Color.fromRGBO(129, 97, 208, 1),
-      const Color.fromRGBO(95, 93, 215, 1),
-      const Color.fromRGBO(76, 93, 220, 1),
-      const Color.fromRGBO(61, 90, 230, 1),
+      const Color.fromRGBO(123, 31, 31, 1),
+      const Color.fromRGBO(207, 48, 27, 1),
+      const Color.fromRGBO(236, 89, 39, 1),
+      const Color.fromRGBO(252, 228, 131, 1),
     ];
-  } else if (index == 3) {
+  }
+  //  카마이클
+  else if (index == 5) {
     return [
-      const Color.fromRGBO(186, 104, 186, 1),
-      const Color.fromRGBO(159, 101, 190, 1),
-      const Color.fromRGBO(129, 97, 208, 1),
-      const Color.fromRGBO(99, 97, 210, 1),
-      const Color.fromRGBO(76, 93, 220, 1),
-      const Color.fromRGBO(61, 90, 230, 1),
+      const Color.fromRGBO(155, 68, 17, 1),
+      const Color.fromRGBO(196, 126, 78, 1),
+      const Color.fromRGBO(239, 207, 113, 1),
+      const Color.fromRGBO(255, 255, 178, 1),
     ];
-  } else if (index == 4) {
+  }
+  //  카이퍼
+  else if (index == 6) {
     return [
-      const Color.fromRGBO(129, 97, 208, 1),
-      const Color.fromRGBO(95, 93, 215, 1),
-      const Color.fromRGBO(76, 93, 220, 1),
-      const Color.fromRGBO(61, 90, 230, 1),
+      const Color.fromRGBO(6, 41, 63, 1),
+      const Color.fromRGBO(0, 63, 95, 1),
+      const Color.fromRGBO(110, 200, 226, 1),
+      const Color.fromRGBO(207, 241, 255, 1),
     ];
-  } else if (index == 5) {
-    return [
-      const Color.fromRGBO(129, 97, 208, 1),
-      const Color.fromRGBO(95, 93, 215, 1),
-      const Color.fromRGBO(76, 93, 220, 1),
-      const Color.fromRGBO(61, 90, 230, 1),
-    ];
-  } else if (index == 6) {
-    return [
-      const Color.fromRGBO(129, 97, 208, 1),
-      const Color.fromRGBO(95, 93, 215, 1),
-      const Color.fromRGBO(76, 93, 220, 1),
-      const Color.fromRGBO(61, 90, 230, 1),
-    ];
-  } else {
+  }
+  //  선택 안됨
+  else {
     return [
       const Color.fromRGBO(129, 97, 208, 0.45),
       const Color.fromRGBO(129, 97, 208, 0.75),
