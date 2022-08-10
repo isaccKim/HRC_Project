e7d5acb208408a4df030a88feadb557f4f0b1e98
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-08-10 17:41:06 +0900

    modified

--- hrc_project/lib/nav_bar/navigation_bar.dart
+++ hrc_project/lib/nav_bar/navigation_bar.dart
@@ -28,8 +28,7 @@ class _NavigationBarState extends State<NavigationBarPage> {
 
   final List<Widget> _pages = [
     const RankingBoardPage(),
-    const TestPage(), //merge시 삭제!
-    //MapSample(),
+    MapSample(),
     const DashBoard(),
     const SettingPage(),
   ];
